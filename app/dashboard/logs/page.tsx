0ed92f05ce5aec6cf670cb1fa6fb98f169d67793
nellanap21 <ap543@student.london.ac.uk>
2025-09-28 22:08:07 +0900

    add search logs

--- app/dashboard/logs/page.tsx
+++ app/dashboard/logs/page.tsx
@@ -1,3 +1,41 @@
-export default function Page() {
-    return <p>Logs Page</p>
+import Pagination from '@/app/ui/logs/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/logs/table';
+import { CreateLog } from '@/app/ui/logs/buttons';
+import { lusitana } from '@/app/ui/fonts';
+import { LogsTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+ 
+// Next.js automatically parses the URL query string (?query=...&page=...) and 
+// passes it as the searchParams prop to your Page component.
+
+// type annotation for the props object that Next.js passes into the page:
+// searchParams is a Promise that resolves to an object with two optional fields, query and string
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams; // waits for searchParams prop to resolve.
+  const query = searchParams?.query || ''; // reads the query parameter from the URL
+  const currentPage = Number(searchParams?.page) || 1; // reads the page parameter from the URL
+  
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Logs</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search logs..." />
+        <CreateLog />
+      </div>
+      <Suspense key={query + currentPage} fallback={<LogsTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense> 
+      <div className="mt-5 flex w-full justify-center">
+        {/* <Pagination totalPages={totalPages} /> */}
+      </div>
+    </div>
+  );
 }
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,16 +1,41 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams(); // returns read-only object representing the query string of the current URL 
+  const pathname = usePathname(); // returns the current pathname
+  const { replace } = useRouter(); // replaces the current URL without adding a new entry to the history stack
+
+  // captures the user's input
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
+
+    {/* creates a mutable copy of the current query string parameters, */}
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300); // debounces the input by 300 milliseconds
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
+      {/* onchange listener invokes handleSearch when input value changes */}
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
