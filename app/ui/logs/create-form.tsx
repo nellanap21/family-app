ceb6c5f0e24a6175251b2f6737ce5211f6d0ce8a
nellanap21 <ap543@student.london.ac.uk>
2025-10-01 17:20:35 -0700

    add form validation to edit log

--- app/ui/logs/create-form.tsx
+++ app/ui/logs/create-form.tsx
@@ -16,7 +16,7 @@ import { useActionState } from 'react';
 export default function Form({ members }: { members: MemberField[] }) {
   const initialState: State = { message: null, errors: {} };
   const [state, formAction] = useActionState(createLog, initialState);
-  console.log(state);
+  
   return <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Member Name */}
--- app/ui/logs/edit-form.tsx
+++ app/ui/logs/edit-form.tsx
@@ -24,7 +24,7 @@ export default function EditLogForm({
   // This will ensure that any values passed to the Server Action are encoded.
   const updateLogWithId = updateLog.bind(null, log.id);
   const [state, formAction] = useActionState(updateLogWithId, initialState);
-
+  console.log(state);
 
   return (
     <form action={formAction}>
@@ -40,6 +40,7 @@ export default function EditLogForm({
               name="memberId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue={log.member_id}
+              aria-describedby="member-error"
             >
               <option value="" disabled>
                 Select a member
@@ -51,6 +52,15 @@ export default function EditLogForm({
               ))}
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+          {/* you can access the errors using the form state. */}
+          <div id="member-error" aria-live="polite" aria-atomic="true">
+          {state.errors?.memberId &&
+            state.errors.memberId.map((error: string) => (
+              <p className="mt-2 text-sm text-red-500" key={error}>
+                {error}
+              </p>
+            ))}
           </div>
         </div>
 
@@ -69,8 +79,19 @@ export default function EditLogForm({
                 defaultValue={log.amount}
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby="amount-error"
+
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+            {/* you can access the errors using the form state. */}
+            <div id="amount-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.amount &&
+              state.errors.amount.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
             </div>
           </div>
         </div>
